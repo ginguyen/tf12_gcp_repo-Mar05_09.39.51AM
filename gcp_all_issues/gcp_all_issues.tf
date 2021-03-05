a45acd80169f06d0dcaf87368c1f5a378952f87d
Giang Nguyen <ginguyen@paloaltonetworks.com>
2021-03-05 09:39:55 -0800

    Modified files:[gcp_all_issues.tf]

--- gcp_all_issues/gcp_all_issues.tf
+++ gcp_all_issues/gcp_all_issues.tf
@@ -350,3 +350,4 @@ resource "google_project_iam_member" "sql_client3" {
     role = "roles/cloudkms.admin"
     member = "user:abc@gmail.com"
 }
+# Modified [gcp_all_issues.tf] on [2021-03-05 09:39:55.201425]
